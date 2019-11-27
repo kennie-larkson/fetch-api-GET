7b799443dd03ec40be99350c550c51766e252622
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2019-11-27 18:23:44 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const ul = document.querySelector('authors');
+const url = 'https://randomuser.me/api/?results=10';
+
+function createNode(element){
+    return document.createElement(element);
+}
+function append(parent, el){
+    return parent.appendChild(el);
+}
+
+
+fetch(url)
+.then((resp) => resp.json())
+.then((data) => {
+    let authors = data.results; //Get the results
+    return authors.map((author) => {
+        let li = createNode('li'); //Create the elements we need
+        let img = createNode('img');
+        let span = createNode('span');
+        img.src = author.picture.medium; //Add the source of the image
+        span.innerHTML = `${author.name.first} ${author.name.last}`;
+        append(li, img);
+        append(li, span);
+        append(ul, li);
+    })
+})
+.catch((error) => { console.log(error);
+});
