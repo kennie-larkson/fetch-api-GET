862dfd469436f94045174030117d7dd9df2a748c
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2019-11-27 18:35:42 +0100

    edited ul selector

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const ul = document.querySelector('authors');
+const ul = document.getElementById('authors');
 const url = 'https://randomuser.me/api/?results=10';
 
 function createNode(element){
@@ -14,9 +14,9 @@ fetch(url)
 .then((data) => {
     let authors = data.results; //Get the results
     return authors.map((author) => {
-        let li = createNode('li'); //Create the elements we need
-        let img = createNode('img');
-        let span = createNode('span');
+        let li = createNode('li'), //Create the elements we need
+            img = createNode('img'),
+            span = createNode('span');
         img.src = author.picture.medium; //Add the source of the image
         span.innerHTML = `${author.name.first} ${author.name.last}`;
         append(li, img);
